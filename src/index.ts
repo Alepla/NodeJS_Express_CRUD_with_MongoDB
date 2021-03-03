3d331e31aaab3bd2e5e7890ce9a373dd1de77560
Alejandro <aleplacambra@gmail.com>
2021-03-03 17:50:07 +0100

    Adding a middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 import mongoose from "mongoose";
 import muestrasRouter from "./muestras/muestras.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -16,6 +18,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api", muestrasRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 const uri: string = `mongodb://127.0.0.1:27017/${process.env.MONGO_DB}`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
