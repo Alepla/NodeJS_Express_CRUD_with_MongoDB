6cbf00928ef497a2f57e8d38c8d32a4729caf6ad
Alejandro <aleplacambra@gmail.com>
2021-03-03 16:40:53 +0100

    addMuestra action

--- src/muestras/muestras.router.ts
+++ src/muestras/muestras.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getMuestras } from "./muestras.service";
+import { getMuestras, addMuestra } from "./muestras.service";
 
 const router: Router = Router();
 
 router.get("/muestras", getMuestras);
+router.post("/add-muestra", addMuestra);
 
 export default router;
--- src/muestras/muestras.service.ts
+++ src/muestras/muestras.service.ts
@@ -14,3 +14,30 @@ export const getMuestras = async (
     res.status(500).send(e.message);
   }
 };
+
+export const addMuestra = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const body = req.body as Pick<
+      IMuestra,
+      "date" | "heartStatus" | "pulse" | "hasECG" | "anomaly" | "user"
+    >;
+
+    const muestra: IMuestra = new Muestra({
+      date: body.date,
+      heartStatus: body.heartStatus,
+      pulse: body.pulse,
+      hasECG: body.hasECG,
+      anomaly: body.anomaly,
+      user: body.user,
+    });
+
+    const newMuestra: IMuestra = await muestra.save();
+
+    res.status(201).json({ muestra: newMuestra });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
